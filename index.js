2a1445e04f73cbd1d79b92b9c44367e4ef866abf
rahul160.jpg <csit191806_rahul@achsnepal.edu.np>
2022-06-21 10:02:29 +0545

    new feature

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ console.log(
   'this is a new feature and is going to sky rocket the whole project...'
 );
 console.log('this repo has quite interesting things pretty awesome...');
+console.log('please ....');
