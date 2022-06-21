cf30f838614f2f463d81eaba05d0b0201186b06a
rahul160.jpg <csit191806_rahul@achsnepal.edu.np>
2022-06-21 09:57:53 +0545

    add new feature in the chat

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 console.log('hello world');
 console.log('added another');
+console.log(
+  'this is a new feature and is going to sky rocket the whole project...'
+);
+console.log('this repo has quite interesting things pretty awesome...');
